b11cece1d31bd46bd667564c8d1efcbae0488122
Hoàng Minh Thắng <p@banphim.net>
2013-11-19 18:46:21 +0700

    initialize project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject angular-sockjs-clojure "0.1.0-SNAPSHOT"
+  :description "A chat application written in Clojure and AngularJS"
+  :url "http://github.com/chlorinejs-demos/angular-sockjs-clojure/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [http-kit-sockjs "0.2.0-SNAPSHOT"]])
