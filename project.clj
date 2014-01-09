d171b9e256a6e31da67dfe1173975224cd656375
Hoàng Minh Thắng <p@banphim.net>
2014-01-09 10:52:50 +0700

    move cl2 source files to src-cl2/

--- project.clj
+++ project.clj
@@ -8,4 +8,26 @@
                  [lib-noir "0.7.5"]
                  [com.taoensso/timbre "2.7.1"]
                  [ring/ring-devel "1.1.8"]]
-  :main angular-sockjs-clojure.core)
+  :main angular-sockjs-clojure.core
+  :aot :all
+  :plugins [[lein-cl2c "0.0.1-SNAPSHOT"]]
+  :cl2c {:client
+         { ;; where to check for changes?
+          :watch ["src-cl2", "test-cl2", "node_modules"]
+          ;; sometimes we don't want to compile all found files
+          ;; but only some of them. Patterns can be combined together
+          ;; with `and` and `or` to make complex filter.
+          :filter (or "src-cl2/" "test-cl2/")
+          ;; - How the compiler figure out its output files from given input?
+          ;; - It's surely a rule
+          :path-map ["src-cl2/" => "resources/public/js/"]
+          ;; where to find cl2 sources?
+          :paths ["node_modules/"]
+          ;; what strategy to use?
+          :strategy "prod"
+          ;; some files may take too long to compile. We need a limit
+          :timeout 2000
+          ;; how are output files optimized?
+          ;; available options: :advanced, :simple (default) and :pretty
+          :optimizations :pretty}}
+  :min-lein-version "2.0.0")
